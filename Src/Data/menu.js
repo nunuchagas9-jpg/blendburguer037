5b70670f18b0c657c715ae9890e1a235bc219945
nunuchagas9-jpg <nunuchagas9@gmail.com>
2026-08-21 14:53:17 -0300

    Adiciona produtos

--- Src/Data/menu.js
+++ Src/Data/menu.js
@@ -2,6 +2,7 @@ export const categories = [
   "ARTESANAIS",
   "TRADICIONAIS",
   "PASTÉIS",
+  "DOCES",
   "ADICIONAIS",
   "COMBOS",
   "BEBIDAS",
@@ -33,7 +34,14 @@ export const menu = [
       "2 blends de 120g, bacon crocante, cheddar, alface, tomate, cebola, catupiry, maionese temperada e batata palha.",
     price: 35,
   },
-
+{
+  id: "x-frango",
+  category: "ARTESANAIS",
+  name: "X-FRANGO",
+  description:
+    "Pão, blend de frango, maionese temperada, alface, tomate e mussarela empanada.",
+  price: 26,
+},
   {
     id: "x-burguer",
     category: "TRADICIONAIS",
@@ -80,7 +88,13 @@ export const menu = [
     description: "Pastel sabor pizza.",
     price: 15,
   },
-
+{
+  id: "hamburguer-doce",
+  category: "DOCES",
+  name: "HAMBÚRGUER DOCE",
+  description: "Pão, creme de avelã e morango.",
+  price: 15,
+},
   {
     id: "mussarela-empanada",
     category: "ADICIONAIS",
