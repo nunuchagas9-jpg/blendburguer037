70fb743181394335527275bba8d9b4ba5a9d6c1b
nunuchagas9-jpg <nunuchagas9@gmail.com>
2026-08-26 11:00:34 -0300

    Remove unused image import from menu.js

--- Src/Data/menu.js
+++ Src/Data/menu.js
@@ -1,4 +1,3 @@
-import xFrangoImage from "../assets/211B5561-4631-40DB-8F7A-AB6E733F48B8.jpeg";
 import doceImage from "../assets/5BC68375-8B4F-4531-A0C0-813A9F725B44.jpeg";
 import blendDuploImage from "../assets/B3B4E00C-F66E-4673-B86F-6281D09EB2FD.png";
 import trufasImage from "../assets/02B95B06-E458-4495-AAF6-B80B272605DB.jpeg";
